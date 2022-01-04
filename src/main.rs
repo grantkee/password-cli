af226a08702672f475dfe2ef93aa9159d0f678e0
grantkee <grant@ether.ai>
2022-01-04 17:55:18 -0600

    cli for parity-wordlist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use parity_wordlist::random_phrase;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Options {
+    #[structopt(default_value="2")]
+    word_count: usize,
+    #[structopt(long, short)]
+    with_hyphens: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let options = Options::from_args(); 
+   let result = match options.with_hyphens {
+       false => random_phrase(options.word_count),
+       true => {
+           let words = random_phrase(options.word_count);
+           str::replace(&words, " ", "-")
+       },
+   };
+    println!("{:?}", result);
 }
